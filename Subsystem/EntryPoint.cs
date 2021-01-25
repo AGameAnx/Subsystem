d3fc35d7234f9913edd8573e4f5a2fad6f07e05c
AGameAnx <agameanx@gmail.com>
2021-01-25 22:24:42 +0200

    Application testing entry point

---
+++ Subsystem/EntryPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Subsystem
+{
+	class EntryPoint
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Test");
+			Console.ReadLine();
+		}
+	}
+}

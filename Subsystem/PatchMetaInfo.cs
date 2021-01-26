99705d37bd7141d6e361dc1f641cba8aa43954eb
AGameAnx <agameanx@gmail.com>
2021-01-27 00:38:11 +0200

    Updated PatchMetaInfo

--- Subsystem/PatchMetaInfo.cs
+++ Subsystem/PatchMetaInfo.cs
@@ -3,11 +3,9 @@ namespace Subsystem
 {
 	public class PatchMetaInfo
 	{
-		public string Name;
-		public string Author;
-		public string Version;
-		public string Description;
-		public string LastUpdate;
-		public string Readme;
+		public string Name = "";
+		public string Author = "";
+		public string Version = "";
+		public string LastUpdate = "";
 	}
 }
